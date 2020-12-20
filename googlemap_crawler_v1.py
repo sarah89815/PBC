b240524c4e2bf0cc74d07d634e8ae5668b3f1e5d
b07701218 <75679450+b07701218@users.noreply.github.com>
2020-12-21 01:49:41 +0800

    Update googlemap_crawler_v1.py
    1. 新增自動找出指定網址功能
    2. 修改商家資訊包含的內容（商家名稱、經度、緯度、平均評價）

--- googlemap_crawler_v1.py
+++ googlemap_crawler_v1.py
@@ -1,23 +1,42 @@
 # coding: utf-8
 
 # 引入函式庫
-import requests 
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import pyautogui
+import requests
 import json
 import csv
 import time
 
-# 商家資訊
-time.sleep(1)
-# 讀入店家資訊連結
-url_1 = input("輸入 Request URL (place?authuser...)\n")
+# 主要用途為取消網頁中的彈出視窗，避免妨礙網路爬蟲的執行。
+options = Options()
+options.add_argument("--disable-notifications")
 
-# 發送 get 請求
-text_1 = requests.get(url_1).text
-# 取代掉特殊字元，這個字元是為了資訊安全而設定
-pretext_1 = ')]}\''
-text_1 = text_1.replace(pretext_1, '')
-# 把字串讀取成 json
-info = json.loads(text_1)
+# 取用 chromedriver
+# 記得改 chromedriver 的位置
+chrome = webdriver.Chrome('/Users/yuchiaching/Desktop/未命名檔案夾/chromedriver', chrome_options=options)
+chrome.maximize_window()
+chrome.get("https://www.google.com.tw/maps")
+
+# 之後還會改
+searchbox_input = chrome.find_element_by_id("searchboxinput")
+searchbox_input.send_keys('YU POKÉ-夏威夷生魚飯')
+searchbox_input.send_keys(Keys.ENTER)
+
+time.sleep(4)
+
+# 商家資訊
+geogCoord_url = chrome.current_url.split(',')
+lat = geogCoord_url[0][-10:]  # 緯度
+lon = geogCoord_url[1]        # 經度
+
+# 餐廳名稱
+store_name = chrome.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[7]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[1]/h1/span[1]").text
+# 平均評價星等
+avg_rating = chrome.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[7]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[1]/div[2]/span/span/span").text
 
 # 追加寫入 store_info.csv 檔
 # 記得改成自己存檔的位置
@@ -27,27 +46,44 @@ with open(file=filepath, mode='a+', encoding='utf-8', newline='') as f1:
 
     writer = csv.writer(f1)
 
-    # 店名
-    store_name = str(info[6][11])
-    # 地址
-    address = str(info[6][2][0][3:])
-    # 電話
-    phone_number = str(info[6][178][0][0])
-    # 總評論數
-    ttl_comment_cnt = str(info[6][4][8])
-    # 平均星等評價
-    avg_rating = str(info[6][4][7])
-    # 星等分佈
-    dist_rating = str(info[6][52][3])
-
-    tmp_list_1 = [store_name, address, phone_number, ttl_comment_cnt, avg_rating, dist_rating]
+    tmp_list_1 = [store_name, lon, lat, avg_rating]
     writer.writerow(tmp_list_1)
 
+# 一些神奇的操作
+btn = chrome.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[7]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[1]/span[1]/span/span/span[2]/span[1]/button")
+btn.click()
+time.sleep(2)
+btn = chrome.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[7]/div/div[1]/div/div/div[2]/div[7]/div[2]/div/button")
+time.sleep(1)
+btn.click()
+time.sleep(1)
+trgt = chrome.find_element_by_id("action-menu").find_elements_by_tag_name("li")[1]
+ActionChains(chrome).double_click(trgt).perform()
+
+# 處理網址
+url_3 = chrome.find_element_by_xpath("/html/body/jsl/div[3]/div[4]/div[1]/ul/li[1]").get_attribute('ved')
+url_3 = url_3.split(':')[4]
+
+time.sleep(3)
+
+cur = "view-source:" + chrome.current_url
+chrome.get(cur)
+
+temp = chrome.find_element_by_xpath("/html/body/table/tbody/tr[155]/td[2]").text
+
+chrome.close()
+
+pos = temp.find('null,[\\"376')
+url_1 = temp[(pos + 8):(pos + 27)]
+pos = temp.find("ludocid\\\\u003")
+url_2 = temp[(pos + 14):(pos + 34)]
+url = ("https://www.google.com.tw/maps/preview/review/listentitiesreviews?authuser=0&hl=zh-TW&gl=tw&pb=!1m2!1y" + 
+       url_1 + "!2y" + url_2 + "!2m2!1i0!2i10!3e2!4m5!3b1!4b1!5b1!6b1!7b1!5m2!1s" + url_3 + "!7e81")
 
 # 商家評論
 
 # 讀入連結
-tmp = input("輸入 Request URL (listentitiesreviews...)\n").split('1i0!')
+tmp = url.split('1i0!')
 
 store_name = store_name.strip(' ')
 store_name = store_name.split(' ')
@@ -95,4 +131,3 @@ with open(file = filepath, mode='a+', encoding='utf-8', newline='') as f2:
             writer.writerow(tmp_list_2)
 
 print("\n" + str(store_name) + "\nFINISH!\n")
-
