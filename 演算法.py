cc6aeda5ebcfcf3d44ad8c3a421fc5199b13b53a
b07701218 <75679450+b07701218@users.noreply.github.com>
2020-12-26 23:00:14 +0800

    Update store_info_final.csv & 演算法.py
    1. 修正 store_info_final.csv 的某些小錯誤
    2. 演算法.py 已經完成先前所說：
    　　a) 將過濾商家函式中「關鍵字」部分改為傳入 list 型態資料
    　　b) 新增以平均星等評價為過濾條件的函式
    3. 要試驗 演算法.py ， 更改第 23 行檔案位置後即可試驗

--- 演算法.py
+++ 演算法.py
@@ -2,66 +2,68 @@ import csv
 
 # 建立商家 class
 class Store:
-    def __init__(self, name, lowerbound, upperbound, area, types):
-        self.name = name                   # 名稱
-        self.lowerbound = int(lowerbound)  # 價錢區間下界
-        self.upperbound = int(upperbound)  # 價錢區間上界
-        self.area = area                   # 區域
-        self.types = types                 # 種類 (是一個清單)
+    def __init__(self, name, lon, lat, avg_ranking, lowerbound, upperbound, area, types):
+        self.name = name                       # 名稱
+        self.lon = float(lon)                  # 經度
+        self.lat = float(lat)                  # 緯度
+        self.avg_ranking = float(avg_ranking)  # 平均評價星等
+        self.lowerbound = int(lowerbound)      # 價錢區間下界
+        self.upperbound = int(upperbound)      # 價錢區間上界
+        self.area = area                       # 區域
+        self.types = types                     # 種類 (是一個清單)
+        # self.keys = keys                       # 關鍵字
 
     def __str__(self):
         return str(self.name)
 
-
 # 所有商家清單
 stores = list()
-# 商家關鍵字清單
-store_keywords = list()
 
 # 記得改成存檔案的地方
 filepath = r'/Users/yuchiaching/Desktop/GitHub/PBC_Final/store_info_final.csv'
 
 with open(file=filepath, mode='r', encoding='utf-8', newline='') as csvfile:
     rows = csv.reader(csvfile)
+    rows = list(rows)
 
-    rows_cnt = 0
-    for row in rows:
+    for i in range(0, len(rows), 2): 
+        # 將價格區間切分為上界和下界儲存
+        price_intl = rows[i][4].split('-')
+        # 轉成 store 類別存入 stores 清單
+        stores.append(Store(rows[i][0], rows[i][1], rows[i][2], rows[i][3], price_intl[0],
+                            price_intl[1], rows[i][5], rows[i][6:len(rows[i])]))
 
-        if rows_cnt % 2 == 0:
-            # 將價格區間切分為上界和下界儲存
-            price_intl = row[4].split('-')
-
-            # 轉成 store 類別存入 stores 清單
-            stores.append(Store(row[0], price_intl[0], price_intl[1], row[5], row[6:len(row)]))
-        else:
-            store_keywords.append(row)
-
-        rows_cnt += 1
 
 # 定義過濾函式
-# 傳入待過濾的商家清單以及關鍵字
-def filter(store_list, keyword):
+# 傳入待過濾的 商家清單 以及 關鍵字清單
+# 這裡是聯集概念
+def filter(store_list, keywords_list):
     # 新增一個空清單
+
     done = list()
 
-    # 檢查每一間商家是否符合條件，符合條件的加入 done 清單
-    for store in store_list:
-        if (keyword != '飯類') and (keyword != '麵類'):
-            if keyword in store.types:
-                done.append(store)
-        else:
-            for each in store.types:
-                # 因為若選擇「麵類」會把有「麵包」的商家也放進來，所以要刪掉    
-                if (keyword[0] in each) and (each != '麵包'):
+    for keyword in keywords_list:
+        # 檢查每一間商家是否符合條件，符合條件的加入 done 清單
+        for store in store_list:
+            if (keyword != '飯類') and (keyword != '麵類'):
+                if keyword in store.types:
                     done.append(store)
-                    break
+            else:
+                for each in store.types:
+                    # 因為若選擇「麵類」會把有「麵包」的商家也放進來，所以要刪掉    
+                    if (keyword[0] in each) and (each != '麵包'):
+                        done.append(store)
+                        break
+
+    # 刪除重複商家
+    done = list(set(done))
 
     # 回傳過濾後的商家清單
     return done
 
 
 # 定義選擇價格區間
-# 傳入待過濾的商家清單以及價格上界
+# 傳入待過濾的 商家清單 以及 價格上界
 def price_choose(store_list, upperbound):
     # 新增一個空清單
     done = list()
@@ -71,13 +73,30 @@ def price_choose(store_list, upperbound):
 
     # 檢查每一間商家是否符合條件，符合條件的加入 done 清單
     for store in store_list:
-        # 如果指定上界在
         if store.upperbound <= upperbound:
             done.append(store)
         elif (store.upperbound > upperbound) and (store.lowerbound < upperbound):
             done.append(store)
         else:
             continue
+
+    # 回傳過濾後的商家清單
+    return done
+
+
+# 定義選擇評價區間
+# 傳入待過濾的 商家清單 以及 評價下界
+def ranking_choose(store_list, lowerbound):
+    # 新增一個空清單
+    done = list()
+
+    # 轉成浮點數
+    lowerbound = float(lowerbound)
+
+    # 檢查每一間商家是否符合條件，符合條件的加入 done 清單
+    for store in store_list:
+        if store.avg_ranking >= lowerbound:
+            done.append(store)
 
     # 回傳過濾後的商家清單
     return done
@@ -92,8 +111,10 @@ def price_choose(store_list, upperbound):
 # print(set(all_type))
 
 # 測試
-print('測試關鍵字，請輸入關鍵字')
+print('測試關鍵字，請輸入關鍵字，可輸入多個以半形空白分割')
 keyword = input()
+
+keyword = keyword.split()
 
 result = filter(stores, keyword)
 
@@ -105,4 +126,11 @@ price = input()
 result_price = price_choose(result, price)
 
 for store in result_price:
+    print(store)
+
+print('測試評價區間，請輸入評價下限')
+ranking = input()
+result_ranking = ranking_choose(result_price, ranking)
+
+for store in result_ranking:
     print(store)
